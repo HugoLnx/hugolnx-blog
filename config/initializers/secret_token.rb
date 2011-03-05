d24b956857e4fed6f872578a9cfc4335a57e5732
HugoLnx <hugolnx@gmail.com>
2011-03-05 09:07:10 -0300

    first commit, new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HugolnxBlog::Application.config.secret_token = 'f66caa96e94e38c6b678074049ceee8797cc0b76fd44991f5ebe851735de34f862924ed4db215c8ad3a35a2aa6eb01c99ec2157a56fc9a61aff1f6e86a94366e'
