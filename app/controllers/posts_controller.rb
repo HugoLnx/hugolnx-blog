bf1ac0040330e57f64a457fe977703fb113e6d50
HugoLnx <hugolnx@gmail.com>
2011-03-07 11:07:35 -0300

    deixando de usar active record

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,7 @@
 class PostsController < ApplicationController
   def show
     id = params[:id]
-    html_lines = File.readlines Dir["app/views/posts/#{sprintf('%.3d',id)}-*"].first
-    @title = html_lines.first.gsub!(/[tT]itle:( *|)/,"")
-    html_lines.delete_at 0
-    @body = html_lines.join.strip
-    render :inline => @body, :layout => true
+    @post = Post.find id
+    render :inline => @post.body, :layout => true
   end
 end
