04a17b8b2ff7a89744463b02e965e370b4614337
HugoLnx <hugolnx@gmail.com>
2011-03-07 12:32:02 -0300

    created a PostFile class to handler the PostFile

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,10 +9,9 @@ class Post
 
   def self.find(id,args={})
     directory = args[:in]
-    html_path = Dir[File.join(directory,"#{sprintf('%.3d',id)}-*")].first
-    title_match = File.basename(html_path).match /^[0-9]{3}\-(.*)\.html(\..*|)$/
-    title = title_match[1].strip
-    body = File.read(html_path).strip
+    post_file = Infraestrutura::PostFile.find id, :in => directory
+    title = post_file.title_from_filename
+    body = post_file.read
     Post.new title, body
   end
 end
