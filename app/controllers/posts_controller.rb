f40ed2070ed0dde1d1f8a55d6cdc8b6f78de663f
HugoLnx <hugolnx@gmail.com>
2011-03-08 11:21:11 -0300

    refactoring structure of views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,6 @@ class PostsController < ApplicationController
   def index
     @id_max = Post.id_max_in 'app/views/posts/posts/'
     @titles = Post.all_post_titles_in 'app/views/posts/posts/'
-    render :nothing => true, :layout => true
   end
 
   def show
