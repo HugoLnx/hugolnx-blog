b776a53538044640926aeb8cefa630a7e0b382a7
HugoLnx <hugolnx@gmail.com>
2011-03-09 15:04:23 -0300

    litte fix

--- config/routes.rb
+++ config/routes.rb
@@ -56,6 +56,6 @@ HugolnxBlog::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
   match 'posts/show(/:id)' => 'posts#show'
-  match 'posts/errors/show(/:id)' => 'errors#show'
+  match 'errors/show(/:id)' => 'errors#show'
   match '/(:id)' => 'posts#index', :constraints => {:id => /\d+/}
 end
