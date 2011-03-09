259f159e23f3ade8040140f8e1411b61c978af52
HugoLnx <hugolnx@gmail.com>
2011-03-08 23:12:17 -0300

    refactoring javascript

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,36 +1,49 @@
+PostSlider = function(elementSelector,warningSelector,titleSelector,max,initialValue,_titles) {
+  var titles = _titles;
+  var element = $(elementSelector);
+  var warningElement = $(warningSelector);
+  var titleElement = $(titleSelector);
+
+  this.updateValueWith = function(newValue) {
+    element.slider({value: newValue});
+  }
+
+  var titleFor = function(ui) {
+    return titles[ui.value-1];
+  }
+
+  var events = {
+    onChange: function(event,ui){
+      warningElement.fadeIn(6000);
+      titleElement.hide();
+      changePostTo(ui.value);
+      document.location.hash = "#"+ui.value;
+    },
+
+    onSliderStartOrSlide: function(event,ui){
+      warningElement.hide();
+      titleElement.show();
+      titleElement.text(titleFor(ui));
+    }
+  }
+
+  element.slider({
+    min: 1,
+    max: max,
+    value: initialValue,
+    change: events.onChange,
+    slide: events.onSliderStartOrSlide,
+    start: events.onSliderStartOrSlide
+  });
+}
 $(document).ready(function(){ 
   recentHash = document.location.hash;
   setInterval(checkChangeOfHash,500);
   var id = catchIdFromHash();
   changePostTo(id);
-  $.syntax() 
-  declareSlider(id_max,id,titles);
+  slider = new PostSlider("#slider","#slider_subtitle span#warning","#slider_subtitle span#title",id_max,id,titles);
 });
 
-function onChange(event,slider){
-  $("#slider_subtitle span#warning").fadeIn(6000);
-  $("#slider_subtitle span#title").hide();
-  changePostTo(slider.value);
-  document.location.hash = "#"+slider.value;
-  $.syntax()
-}
-
-function declareSlider(max,value,titles){
-  $("#slider").slider({
-    min: 1,
-    max: max,
-    value: value,
-    change: onChange,
-    slide: onSliderStartOrSlide,
-    start: onSliderStartOrSlide
-  });
-}
-
-function onSliderStartOrSlide(event,ui){
-  $("#slider_subtitle span#warning").hide();
-  $("#slider_subtitle span#title").show();
-  $("#slider_subtitle span#title").text(titles[ui.value-1]);
-}
 
 function catchIdFromHash(){
   var id = document.location.hash.slice(1);
@@ -42,7 +55,7 @@ function checkChangeOfHash(){
   if (recentHash != document.location.hash){
     var id = catchIdFromHash();
     changePostTo(id);
-    $("#slider").slider({value: id});
+    slider.updateValueWith(id);
   }
 }
 
@@ -52,9 +65,9 @@ function changePostTo(id) {
     url: "/posts/show/",
     dataType: "html",
     data: {id: id},
-    async: false,
-    context: document.body
+    async: false
   }).responseText);
 
   recentHash = document.location.hash;
+  $.syntax()
 }
