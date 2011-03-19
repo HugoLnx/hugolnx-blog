e0da4582e3909e6634f0df8157e69d384e9cd0d6
HugoLnx <hugolnx@gmail.com>
2011-03-19 10:58:13 -0300

    adding authenticity token to post, fixing bug that before comment user is signout from twitter automatically

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,15 @@ class SessionsController < ApplicationController
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"])
     user ||= User.create_with_omniauth(auth)
-    session[:user_id] = user.id
+    session['user_id'] = user.id
     origin = request.env['omniauth.origin']
     origin = File.join(origin,"#" + params['hash']) if params.has_key? 'hash'
     redirect_to origin || '/'
   end
 
   def destroy
-    session[:user_id] = nil
+    pu
+    session['user_id'] = nil
     redirect_to '/'
   end
 
