606bc4d606a8c03be7abe0f962d29e1cd6c8263f
Hugo Roque <hugolnx@gmail.com>
2011-05-23 23:06:54 -0300

    deleting some commented lines

--- spec/requests/show_a_post_spec.rb
+++ spec/requests/show_a_post_spec.rb
@@ -12,10 +12,4 @@ describe 'requests to show a post:' do
     it {should render_template 'posts/show'}
     it {should_not render_template 'layouts/application'}
   end
-
-  #describe 'GET /posts/show/4 (via AJAX)' do
-  #  before(:each) {get '/posts/show/4'}
-  #  it {should render_template 'posts/show'}
-  #  it {should_not render_template 'layouts/application'}
-  #end
 end
