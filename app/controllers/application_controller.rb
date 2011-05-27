24ac3e83a4aec946d21e815f6b3e827f3cfecba0
Hugo Roque <hugolnx@gmail.com>
2011-05-27 15:30:01 -0300

    creating a improvised request log

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :current_user
   before_filter :check_domain
+  before_filter :log_request
 private
   def check_domain
     head :moved_permanently, :location => "http://hugolnx.com/" if request.host == 'hugolnx.heroku.com'
+  end
+
+  def log_request
+    logger = Logger.new('public/requests.log')
+    logger.info "#{request.ip.inspect}|#{Time.now.inspect}|#{request.host.inspect}|#{request.method.inspect}|#{request.fullpath}"
   end
   
   def current_user
