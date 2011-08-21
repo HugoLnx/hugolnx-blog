b4615e1beac4baef778ccf11b073a6a8c46b8ac0
Hugo Roque <hugolnx@gmail.com>
2011-08-21 12:07:27 -0300

    removing bad request logger

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :current_user
   before_filter :check_domain
-  before_filter :log_request
   before_filter :define_robots_metatag_content
 private
   def check_domain
     head :moved_permanently, :location => "http://hugolnx.com/" if request.host == 'hugolnx.heroku.com'
-  end
-
-  def log_request
-    logger = Logger.new('log/requests.log')
-    logger.info "#{request.ip.inspect}|#{Time.now.inspect}|#{request.host.inspect}|#{request.method.inspect}|#{request.fullpath}"
   end
 
   def define_robots_metatag_content
