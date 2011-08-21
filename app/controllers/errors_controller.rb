daf7dafe5c00dcd16e289d609cd030acb9e28cec
Hugo Roque <hugolnx@gmail.com>
2011-08-21 14:58:25 -0300

    refactoration

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,9 +1,8 @@
 # encoding: utf-8
-class ErrorsController < ApplicationController
+class ErrorsController < PostsBaseController
   def show
-    @id_max = Post.id_max
+    prepare_for_slider
     @post = Post.find @id_max
-    @titles = Post.all_post_titles
     error_message = params[:message]
     @message = select_pretty_message_for error_message
     @pagehead = PageHead.of_error
--- app/controllers/posts_base_controller.rb
+++ app/controllers/posts_base_controller.rb
@@ -3,6 +3,10 @@ private
   def prepare_to_render_show_with(post)
     @comments = Comment.find_all_by_post_id(post.id)
     @comment = Comment.new
+    prepare_for_slider
+  end
+
+  def prepare_for_slider
     @id_max = Post.id_max
     @titles = Post.all_post_titles
     @links = links_from @titles
