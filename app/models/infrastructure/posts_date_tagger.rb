771a0536ffc2431b043b4f96c0f760b45d1145bb
Hugo Roque <hugolnx@gmail.com>
2011-08-25 16:04:11 -0300

    tagging by year

--- app/models/infrastructure/posts_date_tagger.rb
+++ app/models/infrastructure/posts_date_tagger.rb
@@ -4,15 +4,7 @@ module Infrastructure
 
     def to_hash(posts)
       tagged_by_year = posts.group_by{|post| post.creation_date.year}
-      actual_year_posts = tagged_by_year.delete(Date.today.year)
-      tagged_by_actual_year_months = group_by_month actual_year_posts
-      posts_tagged = tagged_by_year.merge tagged_by_actual_year_months
-      return posts_tagged
-    end
-
-  private
-    def group_by_month(posts)
-      posts.group_by{|post| post.creation_date.month}
+      return tagged_by_year
     end
   end
 end
--- spec/models/infrastructure/posts_date_tagger_spec.rb
+++ spec/models/infrastructure/posts_date_tagger_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 module Infrastructure
   describe PostsDateTagger do
     describe ".to_hash(posts)" do
-      it "create a hash tagging posts according with last year and months of actual year" do
+      it "create a hash tagging posts by year" do
+        # given
         last_year = Date.today.year-1
         previous_last_year = Date.today.year-2
         last_year_post1 = stub :creation_date => Date.new(last_year,1,23)
@@ -13,10 +14,13 @@ module Infrastructure
         april_post1 = stub :creation_date => Date.new(Date.today.year,4,12)
         april_post2 = stub :creation_date => Date.new(Date.today.year,4,25)
         posts = [previous_last_year_post,last_year_post1,last_year_post2,jan_post,april_post1,april_post2]
+
+        # when
         tagged_posts = PostsDateTagger.to_hash posts
+
+        # then
         tagged_posts.should be == {
-          1 => [jan_post],
-          4 => [april_post1,april_post2],
+          Date.today.year => [jan_post,april_post1,april_post2],
           last_year => [last_year_post1, last_year_post2],
           previous_last_year => [previous_last_year_post]
         }
