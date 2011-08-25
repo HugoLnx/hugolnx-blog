30d373808f84196a563df294295b9236c2b64f5d
Hugo Roque <hugolnx@gmail.com>
2011-08-25 14:25:56 -0300

    using tags system in menu

--- app/controllers/posts_base_controller.rb
+++ app/controllers/posts_base_controller.rb
@@ -3,20 +3,10 @@ private
   def prepare_to_render_show_with(post)
     @comments = Comment.find_all_by_post_id(post.id)
     @comment = Comment.new
-    prepare_for_slider
+    prepare_for_menu
   end
 
-  def prepare_for_slider
-    @id_max = Post.id_max
-    @titles = Post.all_post_titles
-    @links = links_from @titles
-  end
-
-  def links_from(titles)
-    links = []
-    titles.each_with_index do |title,i|
-      links << "#{i+1}-#{title.to_slug.normalize}"
-    end
-    links
+  def prepare_for_menu
+    @tagged_posts = Infrastructure::PostsDateTagger.to_hash Post.all
   end
 end
