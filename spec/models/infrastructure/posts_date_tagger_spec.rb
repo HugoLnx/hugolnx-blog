6e63a4cbf0a28bb503bcde1755e115782e75dadf
Hugo Roque <hugolnx@gmail.com>
2011-08-26 10:39:59 -0300

    refactoring PostsDateTagger spec

--- spec/models/infrastructure/posts_date_tagger_spec.rb
+++ spec/models/infrastructure/posts_date_tagger_spec.rb
@@ -7,22 +7,26 @@ module Infrastructure
         # given
         last_year = Date.today.year-1
         previous_last_year = Date.today.year-2
-        last_year_post1 = stub :creation_date => Date.new(last_year,1,23)
-        last_year_post2 = stub :creation_date => Date.new(last_year,5,15)
-        previous_last_year_post = stub :creation_date => Date.new(previous_last_year,5,15)
-        jan_post = stub :creation_date => Date.new(Date.today.year,1,20)
-        april_post1 = stub :creation_date => Date.new(Date.today.year,4,12)
-        april_post2 = stub :creation_date => Date.new(Date.today.year,4,25)
-        posts = [previous_last_year_post,last_year_post1,last_year_post2,jan_post,april_post1,april_post2]
+        last_year_posts = [
+          stub(:creation_date => Date.new(last_year,1,23)),
+          stub(:creation_date => Date.new(last_year,5,15))
+        ]
+        previous_last_year_posts = [stub(:creation_date => Date.new(previous_last_year,5,15))]
+        actual_year_posts = [
+          stub(:creation_date => Date.new(Date.today.year,1,20)),
+          stub(:creation_date => Date.new(Date.today.year,4,12)),
+          stub(:creation_date => Date.new(Date.today.year,4,25))
+        ]
+        posts = last_year_posts + previous_last_year_posts + actual_year_posts
 
         # when
         tagged_posts = PostsDateTagger.to_hash posts
 
         # then
         tagged_posts.should be == {
-          Date.today.year => [jan_post,april_post1,april_post2],
-          last_year => [last_year_post1, last_year_post2],
-          previous_last_year => [previous_last_year_post]
+          Date.today.year => actual_year_posts,
+          last_year => last_year_posts,
+          previous_last_year => previous_last_year_posts
         }
       end
     end
