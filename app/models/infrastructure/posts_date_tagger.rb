5620e4281890d1f7fa1ffe67e1b978ba95d5bcd4
Hugo Roque <hugolnx@gmail.com>
2011-08-27 23:46:57 -0300

    reorganizing posts tagger algorithm

--- app/models/infrastructure/posts_date_tagger.rb
+++ app/models/infrastructure/posts_date_tagger.rb
@@ -3,9 +3,9 @@ module Infrastructure
     extend self
 
     def to_hash(posts)
-      tagged_by_year = posts.group_by{|post| post.creation_date.year}
-      tagged_by_year = split_bigger_years(tagged_by_year)
-      return arrange(tagged_by_year)
+      tagged_by_year = posts.reverse.group_by{|post| post.creation_date.year}
+      tagged_by_year_and_splited = split_bigger_years(tagged_by_year)
+      return tagged_by_year_and_splited
     end
 
    private
@@ -18,54 +18,18 @@ module Infrastructure
             tagged_and_splited["#{year}/#{i+1}"] = posts_part
           end
         else
-          tagged_and_splited[year] = posts
+          tagged_and_splited[year.to_s] = posts
         end
       end
       return tagged_and_splited
     end
 
     def split_posts_each(size,posts)
-      splited_posts = []
-      while posts.size != 0
-        splited_posts << posts[0..(size-1)]
-        posts = posts.drop size
+      groups = []
+      posts.each_slice(size) do |group|
+        groups = [group] + groups
       end
-      return splited_posts
-    end
-
-    def arrange(tags_hash)
-      arranged = arrange_decreasingly(tags_hash)
-      arranged = arrange_semesters_ascending(arranged)
-      return arranged
-    end
-
-    def arrange_decreasingly(tags_hash)
-      arranged = {}
-      tags_hash.reverse_each do |key,value|
-        arranged[key] = value
-      end
-      return arranged
-    end
-
-    def arrange_semesters_ascending(tags_hash)
-      arranged = {}
-      tags_acumulated = []
-      tags_hash.each_pair do |tag,value|
-        if tag.is_a? Fixnum
-          arranged[tag] = value
-        elsif tag =~ /^\d+\/(\d+)$/
-          semester = $1
-          if semester == "1"
-            arranged[tag] = value
-            tags_acumulated.reverse_each do |(tag,value)|
-              arranged[tag] = value
-            end
-          else
-            tags_acumulated << [tag,value]
-          end
-        end
-      end
-      return arranged
+      return groups
     end
   end
 end
