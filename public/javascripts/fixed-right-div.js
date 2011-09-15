8cd010a09248bb5981e082a385bd7d950dcdeb81
Hugo Roque <hugolnx@gmail.com>
2011-09-15 00:44:48 -0300

    refactoring javascript code

--- public/javascripts/fixed-right-div.js
+++ public/javascripts/fixed-right-div.js
@@ -1,26 +1,5 @@
-$(document).ready(function(){
-  var postRightDiv = $("div#post div#post-right");
-  var initialPostRightDivTop = postRightDiv.offset().top;
-  var initialPostRightDivLeft = postRightDiv.offset().left;
-  var jWindow = $(window);
-  var jDocument = $(document);
-  var postDiv = $("div#post");
-
-  postRightDiv.css("position","absolute");
-  postRightDiv.css("left", initialPostRightDivLeft);
-  postRightDiv.css("top",initialPostRightDivTop + jWindow.scrollTop());
-
-  autoAdjustPostRightDiv();
-
-  jWindow.resize(autoAdjustPostRightDiv);
-  jWindow.scroll(updatePostRightDivTop);
-
-  function autoAdjustPostRightDiv() {
-    var height = jWindow.height() - (postRightDiv.offset().top - jWindow.scrollTop() + (jDocument.height() - (postDiv.offset().top + postDiv.height())));
-    postRightDiv.height(height);
-  }
-
-  function updatePostRightDivTop(){
-    postRightDiv.css("top",initialPostRightDivTop + jWindow.scrollTop());
-  }
-});
+window.onload = function(){
+  var postRightDiv = new PostRightDiv();
+  postRightDiv.fixInInitialPosition();
+  postRightDiv.activeAutoLenghtAdjust();
+};
