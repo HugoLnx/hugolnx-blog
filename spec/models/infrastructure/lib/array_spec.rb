a2ae6105d24aa0b776393654122399c02955052a
Hugo Roque <hugolnx@gmail.com>
2011-09-22 01:17:58 -0300

    refactoring array and string specs

--- spec/models/infrastructure/lib/array_spec.rb
+++ spec/models/infrastructure/lib/array_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper'
 
 describe Array do
-  it 'should pass through it elements adding result of the block' do
-    resultado = [1,2,3,4,5].sum{|number| number * 2 }
-    resultado = 30
+  describe '.sum(&block)' do
+    it 'iterate the elements adding result of the block' do
+      result = [1,2,3,4,5].sum{|number| number * 2 }
+      result = 30
+    end
   end
 end
