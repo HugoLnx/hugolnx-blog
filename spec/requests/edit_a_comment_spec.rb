011ec33a16a3a529cbfa2fd3d1e9cf1a8a31896f
Hugo Roque <hugolnx@gmail.com>
2011-09-22 10:09:03 -0300

    fixing spec bug

--- spec/requests/edit_a_comment_spec.rb
+++ spec/requests/edit_a_comment_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
 
 describe 'requests to edit a comment' do
-  fixtures :users, :posts, :comments
-
   describe 'GET /comments/1/editar?without_layout=true' do
-    before(:each) {get "/comments/#{Comment.first.id}/editar",:without_layout => 'true'}
+    before(:each)  do
+      comment = Comment.create!(:user_id => 1, :post_id => 1, :content => "test")
+      get "/comments/#{comment.id}/editar",:without_layout => 'true'
+    end
     it {should_not render_template 'layouts/application'}
     it {should render_template 'comments/_form'}
   end
