1f06e0347eb8ff589055aafbe0ea7bafc306d4ec
Hugo Roque <hugolnx@gmail.com>
2011-09-22 01:09:23 -0300

    refactoring post_finder spec

--- spec/models/infrastructure/postfile_finder_spec.rb
+++ spec/models/infrastructure/postfile_finder_spec.rb
@@ -2,20 +2,24 @@ require 'spec_helper'
 
 module Infrastructure
   describe PostfileFinder do
-    it 'should find all post titles in spec/fixtures' do
-      paths = PostfileFinder.find_all_in 'spec/fixtures'
-      paths.sort!
-      paths.should be == ['spec/fixtures/001-Testing Post 1.html',
-                          'spec/fixtures/002-Testing Post 2.html']
+    describe '.find_all_in(directory)' do
+      it 'return an array with all post paths in spec/fixtures' do
+        paths = PostfileFinder.find_all_in 'spec/fixtures'
+        paths.sort!
+        paths.should be == ['spec/fixtures/001-Testing Post 1.html',
+                            'spec/fixtures/002-Testing Post 2.html']
+      end
     end
 
-    it 'should find postfile of id 1 in spec/fixtures' do
-      path = PostfileFinder.find(1,'spec/fixtures')
-      path.should be == 'spec/fixtures/001-Testing Post 1.html'
-    end
+    describe '.find(id,directory)' do
+      it 'return post path of id 1 in spec/fixtures' do
+        path = PostfileFinder.find(1,'spec/fixtures')
+        path.should be == 'spec/fixtures/001-Testing Post 1.html'
+      end
 
-    it 'should return nil when try to find postfile 3 in spec/fixtures' do
-      PostfileFinder.find(3,'spec/fixtures').should be_nil
+      it 'return nil when try to find an nonexistent post' do
+        PostfileFinder.find(3,'spec/fixtures').should be_nil
+      end
     end
   end
 end
