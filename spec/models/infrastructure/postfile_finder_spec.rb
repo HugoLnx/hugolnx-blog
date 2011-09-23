53fe7e6ba88caa73fff011910471b70327ea507a
Hugo Roque <hugolnx@gmail.com>
2011-09-23 11:26:27 -0300

    fixing spec

--- spec/models/infrastructure/postfile_finder_spec.rb
+++ spec/models/infrastructure/postfile_finder_spec.rb
@@ -6,7 +6,7 @@ module Infrastructure
       it "return an array with all post paths in TEST_POSTS_DIRECTORY/*" do
         paths = PostfileFinder.find_all_in TEST_POSTS_DIRECTORY
         paths.sort!
-        paths.should be == TEST_POSTS_IN_TYPE_1
+        paths.should be == TEST_POSTS_IN_TYPE_1 + TEST_POSTS_IN_TYPE_2
       end
     end
   end
