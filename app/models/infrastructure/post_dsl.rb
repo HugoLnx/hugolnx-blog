f4fc001241922d6a2b0e8cabefc5708a68eeed8a
Hugo Roque <hugolnx@gmail.com>
2011-09-23 02:17:02 -0300

    indexing posts right

--- app/models/infrastructure/post_dsl.rb
+++ app/models/infrastructure/post_dsl.rb
@@ -21,11 +21,26 @@ module Infrastructure
 
     def build_all_in(directory)
       paths = PostfileFinder.find_all_in directory
-      posts = paths.collect.with_index do |post_path,id|
-        postfile, postfile_content = wrap_postfile_from(post_path)
-        build(postfile,postfile_content,id)
+      postfiles = paths.collect do |post_path|
+        wrap_postfile_from(post_path)
+      end
+      postfiles = sort_by_indexing_order(postfiles)
+
+      posts = postfiles.collect.with_index do |(file,content),id|
+        build(file,content,id)
       end
       return posts
+    end
+
+    def sort_by_indexing_order(postfiles)
+      postfiles.sort do |(file_a,content_a),(file_b,content_b)|
+        attrs_a = content_a.catch_attributes
+        attrs_b = content_b.catch_attributes
+        result = attrs_a[:creation_date] <=> attrs_b[:creation_date]
+        result = file_a.location <=> file_b.location if result.zero?
+        result = file_a.id <=> file_b.id if result.zero?
+        result
+      end
     end
   end
 end
