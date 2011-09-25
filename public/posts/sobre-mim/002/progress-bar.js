d6a19afd140faf61245c514b01fe4c2d340d013e
Hugo Roque <hugolnx@gmail.com>
2011-09-25 11:49:33 -0300

    knowledge portfolio page

---
+++ public/posts/sobre-mim/002/progress-bar.js
@@ -0,0 +1,54 @@
+window.onload = function() {
+  var GREAT = {
+    TOPBAR: "rgba(0,0,255,0.5)",
+    BOTTOMBAR: "#009"
+  };
+
+  var GOOD = {
+    TOPBAR: "rgba(0,255,0,0.5)",
+    BOTTOMBAR: "#090"
+  };
+
+  var BAD = {
+    TOPBAR: "rgba(255,0,0,0.5)",
+    BOTTOMBAR: "#900"
+  };
+
+  (function(){
+    var progressBars = $("div.progress-bar");
+    progressBars.each(function(i){
+      var progressBar = $(progressBars[i]);
+      fillBar = $(document.createElement('div'));
+      fillTopBar = $(document.createElement('div'));
+      fillBottomBar = $(document.createElement('div'));
+
+      fillBar.width(progressBar.attr("data-progress"));
+      fillBar.height("100%");
+
+      fillTopBar.height("60%");
+      fillTopBar.width("100%");
+
+      var porcent = parseInt(progressBar.attr("data-progress").slice(0,-1));
+
+      var fillColor;
+      if (porcent > 80) {
+        fillColor = GREAT;
+      } else if (porcent > 40) {
+        fillColor = GOOD;
+      } else {
+        fillColor = BAD;
+      }
+      fillTopBar.css("background-color",fillColor.TOPBAR);
+      fillBar.css("background-color",fillColor.BOTTOMBAR);
+
+      fillBar.append(fillTopBar);
+
+      progressBar.append(fillBar);
+
+      var progressStatus = $(document.createElement('span'));
+      progressStatus.text(progressBar.attr("data-progress"));
+
+      progressBar.append(progressStatus);
+    });
+  })();
+};
