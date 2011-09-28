7d9b856160a9c0229c8f2d6ba15b3d18fb887d19
Hugo Roque <hugolnx@gmail.com>
2011-09-28 11:11:10 -0300

    limiting length of comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,5 @@ class Comment < ActiveRecord::Base
   validates_presence_of :post_id
   validates_presence_of :user_id
   validates_presence_of :content
+  validates_length_of :content, :maximum => 1600
 end
