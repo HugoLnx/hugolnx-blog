5a3780b4e3adf2c2bd5ef62156f16ea47fb94167
Hugo Roque <hugolnx@gmail.com>
2011-09-29 02:05:04 -0300

    sharing services heads update with ajax requests

--- app/controllers/without_layout/posts_controller.rb
+++ app/controllers/without_layout/posts_controller.rb
@@ -4,8 +4,20 @@ class WithoutLayout::PostsController < PostsBaseController
   def show
     id = params[:id].sub('/','').match(/\d+/)[0]
     @post = Post.find id
+    @post_url = post_url(@post.friendly_id)
+
+    @sharing_head = Infrastructure::SharingServicesHead.new(
+      :post => @post,
+      :request => request,
+      :url => @post_url
+    )
     prepare_to_render_show_with @post
-    render 'posts/show'
-    response.headers['title'] = @post.title
+    render 'posts/show.html.erb'
+    response.content_type = "application/json"
+    response.body = {
+      :body => response.body,
+      :title => @post.title,
+      :sharing_head => @sharing_head.to_hash
+    }.to_json
   end
 end
