c72d5e6063c3b6a9b07cf606964c2944b60d119f
Hugo Roque <hugolnx@gmail.com>
2011-09-29 09:57:59 -0300

    responding with json in without_layout/other_posts_controller too

--- app/controllers/without_layout/other_posts_controller.rb
+++ app/controllers/without_layout/other_posts_controller.rb
@@ -1,15 +1,15 @@
-class WithoutLayout::OtherPostsController < PostsBaseController
-  layout nil
+module WithoutLayout
+  class OtherPostsController < PostsBaseController
+    layout nil
 
-  def show
-    location = params[:location]
-    friendly_title = params[:id]
-    @post = Post.find(:location => location, :friendly_title => friendly_title)
-    @post_url = post_url @post.friendly_id
+    def show
+      location = params[:location]
+      friendly_title = params[:id]
+      @post = Post.find(:location => location, :friendly_title => friendly_title)
+      @post_url = post_url @post.friendly_id
 
-    prepare_to_render_show_with @post, @post_url
-
-    render 'posts/show'
-    response.headers['title'] = @post.title
+      prepare_to_render_show_with @post, @post_url
+      render_json_to_page_changes
+    end
   end
 end
--- app/controllers/without_layout/posts_controller.rb
+++ app/controllers/without_layout/posts_controller.rb
@@ -1,19 +1,14 @@
-class WithoutLayout::PostsController < PostsBaseController
-  layout nil
+module WithoutLayout
+  class PostsController < PostsBaseController
+    layout nil
 
-  def show
-    id = params[:id].sub('/','').match(/\d+/)[0]
-    @post = Post.find id
-    @post_url = post_url(@post.friendly_id)
+    def show
+      id = params[:id].sub('/','').match(/\d+/)[0]
+      @post = Post.find id
+      @post_url = post_url(@post.friendly_id)
 
-    prepare_to_render_show_with @post, @post_url
-
-    render 'posts/show.html.erb'
-    response.content_type = "application/json"
-    response.body = {
-      :body => response.body,
-      :title => @post.title,
-      :sharing_head => @sharing_head.to_hash
-    }.to_json
+      prepare_to_render_show_with @post, @post_url
+      render_json_to_page_changes
+    end
   end
 end
